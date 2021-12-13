b51450aa8a6e969c7dae1778b081dcceac17e4fc
mraasvel <mbraasveldt@gmail.com>
2021-12-13 21:35:07 +0100

    program arg check

--- base/src/main.cpp
+++ base/src/main.cpp
@@ -6,6 +6,7 @@ typedef util::Point<int> Point;
 typedef util::HashPoint<int> HashPoint;
 
 int main(int argc, char *argv[]) {
+	assert(argc != 1);
 	std::ifstream file(argv[1]);
 	assert(file.is_open());
 	std::string line;
