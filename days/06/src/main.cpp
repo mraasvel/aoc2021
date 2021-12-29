c3b459544828875d7cefb6ee8542251d760ccc29
mraasvel <mbraasveldt@gmail.com>
2021-12-29 22:31:14 +0100

    fix day 6

--- days/06/src/main.cpp
+++ days/06/src/main.cpp
@@ -9,124 +9,60 @@
 #include <set>
 #include <unordered_map>
 #include <map>
+#include <sstream>
 
 #define FILENAME "input.txt"
 // #define FILENAME "sample.txt"
 
 using namespace std;
 
-struct Fish {
-
-	Fish(size_t time = 8) : timer(time), num_fish(1) {}
-
-	// return true if new fish should be created
-	bool increment() {
-		if (timer == 0) {
-			timer = 6;
-			return true;
-		} else {
-			timer -= 1;
-		}
-		return false;
-	}
-
-	size_t timer;
-	size_t num_fish;
-};
-
 #define DAYS 256
 
-/*
-1. Decremting: we want to change the number to the key [key - 1]: however if this key is already taken and not processed yet, it should be processed first before we replace the value
-2. 
-*/
-struct Fishies {
-	typedef map<int, size_t> map_type;
-	// typedef unordered_map<int, size_t> map_type;
-	Fishies() : total(0) {}
-
-	void add(const Fish& fish) {
-		auto it = m.find(fish.timer);
-		if (it == m.end()) {
-			m[fish.timer] = 1;
+std::vector<size_t> nextCycle(const std::vector<size_t>& fish) {
+	std::vector<size_t> next(10, 0);
+	for (std::size_t i = 0; i < fish.size(); i++) {
+		if (i == 0) {
+			next[6] += fish[i];
+			next[8] += fish[i];
 		} else {
-			m[fish.timer] += 1;
-		}
-		total += 1;
-	}
-
-	void print() {
-		for (auto f : m) {
-			cout << f.first << " (" << f.second << ") ";
-		}
-		cout << "Total: " << total << endl;
-	}
-
-	void execDay() {
-		size_t to_add = 0;
-		for (auto& it  : m) {
-			size_t num = it.second;
-			if (it.first == 0) {
-				to_add += num;
-				addNew(6, num);
-			} else {
-				addNew(it.first - 1, num);
-			}
-		}
-		addFishies(to_add);
-		m.clear();
-		new_map.swap(m);
-	}
-
-	void addNew(int time, size_t n) {
-		auto it = new_map.find(time);
-		if (it == new_map.end()) {
-			new_map[time] = n;
-		} else {
-			new_map[time] += n;
+			next[i - 1] += fish[i];
 		}
 	}
+	return next;
+}
 
-	void addFishies(size_t n) {
-		if (n == 0) {
-			return;
+void printCycle(const std::vector<size_t>& fish) {
+	std::cout << "Cycle: " << std::endl;
+	for (size_t count : fish) {
+		std::cout << count << ' ';
+	}
+	std::cout << std::endl;
+}
+
+void printTotalFish(const std::vector<size_t>& fish) {
+	size_t total = 0;
+	for (size_t count : fish) {
+		total += count;
+	}
+	std::cout << total << endl;
+}
+
+int main(int argc, char *argv[]) {
+	std::vector<size_t> fish(10, 0);
+	assert(argc != 1);
+	std::ifstream ifs(argv[1]);
+	assert(ifs.is_open());
+	while (!ifs.eof()) {
+		if (!isdigit(ifs.peek())) {
+			break;
 		}
-		addNew(8, n);
-		total += n;
+		int n = ifs.get() - '0';
+		fish[n] += 1;
+		ifs.ignore(1);
 	}
-
-	size_t count() const {
-		return total;
+	for (size_t i = 0; i < DAYS; i++) {
+		fish = nextCycle(fish);
 	}
-
-	// map timer to number of fish;
-	map_type m;
-	map_type new_map;
-	size_t total;
-};
-
-int main() {
-
-	vector<Fish> fish;
-	Fishies fishies;
-
-	std::ifstream file(FILENAME);
-	assert(file.is_open());
-	std::string line;
-	while (std::getline(file, line)) {
-		auto v = util::splitString(line, ',');
-		for (auto val : v) {
-			Fish fishy = Fish(stoull(val));
-			fishies.add(fishy);
-		}
-	}
-	file.close();
-
-	for (size_t i = 0; i < DAYS; ++i) {
-		// fishies.print();
-		fishies.execDay();
-		// cout << i+ 1 << ": " << fishies.count() << endl;
-	}
-	cout << fishies.count() << endl;
+	printTotalFish(fish);
 	return 0;
 }
